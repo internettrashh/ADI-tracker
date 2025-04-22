bc05ba3d3fab0f100d29275f623ba1ed50c764d9
internet_trash <naiknischal92@gmail.com>
2025-04-22 15:31:58 +0530

    risk hai to isk hai

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -23,6 +23,14 @@ export default tseslint.config(
         "warn",
         { allowConstantExport: true },
       ],
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/explicit-module-boundary-types": "off",
+      "@typescript-eslint/no-non-null-assertion": "off",
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-inferrable-types": "off",
+      "@typescript-eslint/ban-ts-comment": "off",
+      "@typescript-eslint/no-empty-function": "off",
     },
   }
 );
